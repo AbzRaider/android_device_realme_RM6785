e0fd063ffd2a136954fe23c852b659691a19faea
Matsvei Niaverau <matvej2002@gmail.com>
2023-08-13 12:57:48 +0530

    RM6785: {device, overlay}: Build the PowerOffAlarm package
    Import and start building the PowerOffAlarm package into the rom, and also update the default driver to RTC and keep the TimerFD as the backup option.
    
    Co-authored-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -263,6 +263,10 @@ PRODUCT_PACKAGES += \
 -include $(LOCAL_PATH)/properties.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
+# PowerOffAlarm
+PRODUCT_PACKAGES += \
+    PowerOffAlarm
+
 # Properties
 include $(LOCAL_PATH)/configs/props/vendor_prop.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
