4cea154c28de21a762e0172075dd61ecc7a6db7b
SamarV-121 <samarvispute121@pm.me>
2023-09-17 13:22:04 +0000

    RM6785 : Adding Performance Performance Profiles

--- app/RealmeParts/parts.mk
+++ app/RealmeParts/parts.mk
@@ -1,12 +1,13 @@
-DEVICE_PATH := device/realme/RMX2020
+DEVICE_PATH := device/realme/RM6785
 
 # Init scripts
 PRODUCT_PACKAGES += \
-        parts.rc
+    parts.rc
 
 # Parts
 PRODUCT_PACKAGES += \
     RealmeParts
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/app/RealmeParts/init/cabc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/cabc.rc
+    $(DEVICE_PATH)/app/RealmeParts/init/cabc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/cabc.rc \
+    $(DEVICE_PATH)/app/RealmeParts/init/perf_profile.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/perf_profile.sh 
