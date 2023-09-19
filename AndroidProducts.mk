644bbe576754605f266ba16088a4b54b68efd58f
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-09-19 12:37:25 +0000

    RM6785 : PixysOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RM6785.mk
+    $(LOCAL_DIR)/pixys_RM6785.mk
+
+COMMON_LUNCH_CHOICES := \
+    pixys_RM6785-user \
+    pixys_RM6785-userdebug \
+    pixys_RM6785-eng
--- device.mk
+++ device.mk
@@ -201,7 +201,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay_pixys
 
 PRODUCT_PACKAGES += \
     FrameworksOverlayRMX2151 \
